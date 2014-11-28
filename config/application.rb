e565cb5498851533bdb8a80e10e2c11d2dcfa57a
Piotr Misiurek <p.misiurek@gmail.com>
2014-11-28 22:11:30 +0100

    Polish locale

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module BudzetObywatelski
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.locale = :pl
+    config.i18n.default_locale = :pl
+
   end
 end
